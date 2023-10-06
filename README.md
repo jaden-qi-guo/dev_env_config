3bcedd77a1c0302120c11cbf254131cecc8163a4
Qi Guo <qiguo@mpi-inf.mpg.de>
2023-10-06 14:31:42 +0200

    Github Copilot nvim support

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dev_env_config
 This is the repo where I maintain the dev_env_config.
+
+
+## Manual Configuration
+
+
+### Neovim
+
+```
+:Copilot setup
+```
+
--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
 # This will be a one-shot shell scripts which will help configure all the dev env config
+
+
+# Github Copilot
+git clone https://github.com/github/copilot.vim.git \
+  ~/.config/nvim/pack/github/start/copilot.vim
